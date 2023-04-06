f1e7ada3fd38292008a0b52cd53c33be3489b9ef
Ibrahim <iimran@greenlightiot.com>
2023-04-06 16:56:23 -0400

    implemented add/delete course

--- src/app/pages/course-page/course-page.component.ts
+++ src/app/pages/course-page/course-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ColDef, GridApi } from 'ag-grid-community';
+import { MatDialog } from '@angular/material/dialog';
+import { ColDef, GridApi, RowSelectedEvent } from 'ag-grid-community';
+import { AddCourseDialogComponent } from 'src/app/features/course/components/add-course-dialog/add-course-dialog.component';
+import { CourseT } from 'src/app/features/course/interfaces/course.interface';
 
 @Component({
   selector: 'app-course-page',
@@ -7,17 +10,21 @@ import { ColDef, GridApi } from 'ag-grid-community';
   styleUrls: ['./course-page.component.scss'],
 })
 export class CoursePageComponent {
-  public rowData;
+  public selectedRowCount = 0;
+
+  private gridApi: GridApi | undefined;
+
+  public rowData: CourseT[];
   public columnDefs: ColDef[] = this.getColumnDefs();
   public defaultColDef: ColDef = this.getDefaultColDef();
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.rowData = [
       {
         courseID: 'CSC 436',
         title: 'Software Engineering',
         instructor: 'Dr. Kiper',
-        credits: '3',
+        credits: 3,
       },
       {
         courseID: 'CSC 437',
@@ -34,6 +41,10 @@ export class CoursePageComponent {
     ];
   }
 
+  onGridReady(params: any): void {
+    this.gridApi = params.api;
+  }
+
   getColumnDefs(): ColDef[] {
     return [
       { field: 'courseID', headerName: 'Course ID', width: 150 },
@@ -45,5 +56,31 @@ export class CoursePageComponent {
 
   getDefaultColDef(): ColDef {
     return { sortable: true, resizable: true };
+  }
+
+  openAddCourseDialog(): void {
+    const dialogRef = this.dialog.open(AddCourseDialogComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      this.rowData.push(result);
+      this.gridApi?.setRowData(this.rowData);
+    });
+  }
+
+  openDeleteCourseDialog(): void {
+    const selectedNodes = this.gridApi?.getSelectedNodes();
+    if (selectedNodes) {
+      const selectedData = selectedNodes.map((node) => node.data);
+      this.rowData = this.rowData.filter(
+        (course) => !selectedData.includes(course)
+      );
+      this.gridApi?.setRowData(this.rowData);
+      this.updateSelectedRowCount();
+    }
+  }
+
+  updateSelectedRowCount(): void {
+    if (this.gridApi) {
+      this.selectedRowCount = this.gridApi.getSelectedNodes().length;
+    }
   }
 }
